498dddfe983a6adc69dd493a0ffbb65b6c34feac
mor3b554665 <102178779+mor3b554665@users.noreply.github.com>
2024-01-05 17:45:07 +0200

    Update settings.py

--- AnonX/utils/inline/settings.py
+++ AnonX/utils/inline/settings.py
@@ -24,7 +24,7 @@ def setting_markup(_):
         ],
         [
             InlineKeyboardButton(
-                text="⌞ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙳𝙰𝚁𝙺� ⌝", url=f"https://t.me/T4_Mohamed",
+                text="⌞ 𝚂𝙾𝚄𝚁𝙲𝙴 ᴍᴏʀзʙ ⌝", url=f"https://t.me/UC_IU",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
