14670d8ea68c96f481999eba5481054a4915352b
mor3b554665 <102178779+mor3b554665@users.noreply.github.com>
2024-01-06 15:27:33 +0200

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text=" ⌞ قناه البوت ⌝ ", url=f"https://t.me/I1_35"
+                text=" قناه البوت  ", url=f"https://t.me/I1_35"
             )
         ],
      ]
