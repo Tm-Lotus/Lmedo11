ea4d0bb4f1f00601c96295db538dc72e66aa0852
mor3b554665 <102178779+mor3b554665@users.noreply.github.com>
2024-01-06 11:16:00 +0200

    Update zagrapha.py

--- AnonX/plugins/zagrapha.py
+++ AnonX/plugins/zagrapha.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from AnonX import app
 
-@app.on_message(filters.command("زخرفة", ""))
+@app.on_message(filters.command("زخرفه", ""))
 async def zhakhrafa(_, message: Message):
     data = message.text.split(maxsplit=1)
     if len(data) < 2: return await message.reply("- ازخرف اي؟؟", reply_to_message_id=message.message_id)
